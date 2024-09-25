a3f953c791813cef829fbd4cc59e20cae74d79b9
rishiriii <rishiriii.10@gmail.com>
2024-09-25 17:51:28 +0530

    Delete asso.pl

--- asso.pl
+++
@@ -1,7 +0,0 @@
-student(raj).
-student(rahul).
-hscstudent(raj).
-sscstudent(raj).
-sscstudent(rahul).
-lbscitadmission(X):-((student(X),hscstudent(X)),sscstudent(X)).
-rbscitadmission(X):-(student(X),(hscstudent(X),sscstudent(X))).
